779eda3d1f51f561ef50a52564c3c451c3f6b97e
Aishwarya Bachchani <160261613+aiishwaryaa@users.noreply.github.com>
2025-03-12 19:13:03 +0530

    bulb using javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+//turn on fnc
+function bulbon(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute('src','light.png')
+    bulbobj.setAttribute('style','width:250px; height:400px' )
+
+ }
+ function bulbOff(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute('src','bulb-removebg-preview.png')
+    bulbobj.setAttribute('style','width:420px; height:400px' )
+
+ }
